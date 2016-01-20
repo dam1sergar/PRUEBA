011a44f682ca2d392efd009318e46391f3d60471
asus <asus@192.168.1.162>
2016-01-20 18:40:06 +0100

    este es el testing

--- github/src/github/Github.java
+++ github/src/github/Github.java
@@ -16,6 +16,7 @@ public class Github {
      */
     public static void main(String[] args) {
         System.out.println("ESTA ES UNA PRACTICA DE GITHUB");
+        System.out.println("MODIFICACION");
     }
     
 }
